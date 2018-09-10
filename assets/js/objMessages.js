a14c781870ea26354a12358e66064058914caebc
Krystina Hamel <hkrystina@hotmail.ca>
2018-09-10 14:53:22 -0400

    Setup CSS et ajout des fichiers typescripts

--- assets/js/objMessages.js
+++ assets/js/objMessages.js
@@ -5,40 +5,43 @@ const objMessages = {
             "motif": "Explication complète du motif attendu"
         }
     },
-
     "jeSuis": {
         "erreurs": {
             "vide": "Veuillez préciser votre identité de genre, s’il-vous-plaît."
         },
         "ok": "Parfait! passons à l'étape suivante..."
     },
-
     "jeCherche": {
         "erreurs": {
             "vide": "Veuillez préciser votre recherche, s’il-vous-plaît."
         },
         "ok": "Parfait! passons à l'étape suivante..."
     },
-
-    "dateNaissance": {
+    "jour": {
         "erreurs": {
-            "vide": "Veuillez entrer votre date de naissance, s’il-vous-plaît. ",
-            "motif": "Vous êtes trop jeune pour vous inscrire ici!",
-            "type": {
-                "jour": "Entrez votre jour de naissance",
-                "mois": "Sélectionnez votre mois de naissance",
-                "annee": "Entrez les 4 chiffres de votre année de naissance"
-            }
+            "vide": "Entrez votre jour de naissance",
+            "motif": "Il semble y avoir une erreur dans la saisie de votre jour de naissance"
         }
     },
-
+    "mois": {
+        "erreurs": {
+            "vide": "Sélectionnez votre mois de naissance",
+            "motif": "Il semble y avoir une erreur dans la saisie de votre mois de naissance"
+        }
+    },
+    "annee": {
+        "erreurs": {
+            "vide": "Entrez les 4 chiffres de votre année de naissance",
+            "motif": "Il semble y avoir une erreur dans la saisie de votre année de naissance",
+            "age": "Vous êtes trop jeune pour vous inscrire ici!"
+        }
+    },
     "codePostal": {
         "erreurs": {
             "vide": "Veuillez entrer votre code postal pour nous permettre d'identifier votre lieu de résidence.",
             "motif": "Entrez votre code postal au format A1A1A1, sans espace."
         }
     },
-
     "pseudo": {
         "erreurs": {
             "vide": "Veuiller compléter le champ pseudo, s’il-vous-plaît.",
@@ -46,14 +49,12 @@ const objMessages = {
             "motif": "Attention, votre nom d'usager doit comporter un minimum de 2 lettres. Trait d'union ou espace sont permis pour séparer deux mots."
         }
     },
-
     "courriel": {
         "erreurs": {
             "vide": "Veuillez entrer votre adresse courriel, s’il-vous-plaît.",
             "motif": "Vérifiez votre adresse courriel, il semble y avoir une erreur."
         }
     },
-
     "motDePasse": {
         "erreurs": {
             "vide": "Veuillez définir votre mot de passe.",
@@ -64,7 +65,6 @@ const objMessages = {
             "num": "Votre mot de passe doit comporter au moins un chiffre"
         }
     },
-
     "consentement": {
         "erreurs": {
             "vide": "SVP, veuillez prendre connaissance et approuvez les termes de l'entente."
